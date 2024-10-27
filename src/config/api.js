42d695b977eb36c811f3b6a8cfebd1d5b23a6a00
Marco2aa <bmarcantoine@outlook.fr>
2024-10-27 11:21:59 +0100

    vercel commit

--- src/config/api.js
+++ src/config/api.js
@@ -11,7 +11,7 @@ export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
 
 export const GetDataBaseCoins = (nbPage) =>
-  `http://35.181.62.85/api/cryptos?page=${nbPage}`
+  `${BASE_URL}/api/cryptos?page=${nbPage}`
 
 export const BASE_URL = "https://afflokat-marc.store/public"
 
